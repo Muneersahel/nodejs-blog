41a4e1c597c0e34c51b50a8f58fd16d8dbb223e0
Muneersahel <saidmunir73@gmail.com>
2021-06-22 18:31:38 +0300

    modify project structure

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes')
+
+// express app
 const app = express();
+
+const PORT = 3000;
+
+// connect to mongodb
+const dbURI = 'mongodb+srv://nodejs-user:aPw9mVLWR7Rz9ZW@nodejs-blogs.dj90m.mongodb.net/nodejs-blogs?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        console.log('connected to db\nPORT: ' + PORT);
+        app.listen(PORT);
+    })
+    .catch((err) => console.log(err));
+
+
 // register view engine
 app.set('view engine', 'ejs')
 
-app.listen(3000);
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
 
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Blog 1', snippet: 'lorem ipsum dolor sit amet consectetur'},
-        {title: 'Blog 2', snippet: 'lorem ipsum dolor sit amet consectetur'},
-        {title: 'Blog 3', snippet: 'lorem ipsum dolor sit amet consectetur'},
-    ];
-    res.render('index', {title: 'Home', blogs: blogs});
+    res.redirect('/blogs');
 });
+
+// blog routes
+app.use('/blogs' ,blogRoutes);
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'})
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create a new Blog'})
-})
 app.use((req, res) => {
     res.status(404).render('404');
 });
