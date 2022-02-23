22aa0eccea2a419a7a459abc8d4609e581b6697a
Muneersahel <saidmunir73@gmail.com>
2022-02-24 00:09:28 +0300

    modify app

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const blogRoutes = require("./routes/blogRoutes");
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
@@ -9,35 +10,34 @@ const app = express();
 const PORT = 3000;
 
 // connect to mongodb
-const dbURI =
-  "mongodb+srv://node-user:aPw9mVLWR7Rz9ZW@cluster0.ub01n.mongodb.net/nodejs-blog?retryWrites=true&w=majority";
+const dbURI = process.env.MONGO_URI;
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
-    console.log("connected to db\nPORT: " + PORT);
+  .then(() => {
+    console.log('connected to db\nPORT: ' + PORT);
     app.listen(PORT);
   })
   .catch((err) => console.log(err));
 
 // register view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // middleware & static files
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.get("/", (req, res) => {
-  res.redirect("/blogs");
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
 
 // blog routes
-app.use("/blogs", blogRoutes);
+app.use('/blogs', blogRoutes);
 
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render('404');
 });
